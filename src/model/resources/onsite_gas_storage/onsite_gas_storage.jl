75183c848f4db78640d683126a88ed025c868f77
Romain Teyssier <teyssier@princeton.edu>
2023-12-15 16:05:22 -0500

    Polished onsite storage module

--- src/model/resources/onsite_gas_storage/onsite_gas_storage.jl
+++ src/model/resources/onsite_gas_storage/onsite_gas_storage.jl
@@ -1,108 +1,131 @@
+"""
+    onsite_gas_storage!(EP::Model, inputs::Dict, setup::Dict)
+
+Integrates an onsite gas storage module into the optimization model 'EP'.
+
+## Description
+This function adds variables, expressions, and constraints related to onsite gas storage to the energy production model `EP`. It utilizes input data and configuration parameters from `inputs` and `setup` dictionaries.
+
+## Arguments
+- `EP::Model`: The optimization model to which the onsite gas storage functionality is added.
+- `inputs::Dict`: A dictionary containing input data such as:
+  - `dfGen`: Dataframe with generators data.
+  - `T`: Number of time steps (hours).
+  - `Z`: Number of zones.
+  - `hours_per_subperiod`: Total number of hours per subperiod.
+  - Other required parameters for gas, oil, and hydrogen storage calculation.
+- `setup::Dict`: A dictionary containing setup configurations like the types of storage (`CNGStorage`, `OilStorage`, `HydrogenStorage`) and their associated flags.
+
+## Functionality
+- Defines variables for storage capacity, charge, discharge, and state of storage for each gas plant.
+- Calculates fixed and variable costs associated with different types of gas storages (CNG, oil, hydrogen).
+- Adds these costs to the objective function of the `EP` model.
+- Imposes constraints for state of storage, discharge, minimum state of storage, maximum state of storage, and maximum charge rate depending on the type of storage and other model parameters.
+
+## Output
+- Modifies the `EP` model by adding new variables, constraints, and objective function components related to onsite gas storage.
+- Prints confirmation upon successful integration of the module.
+
+## Example
+```julia
+onsite_gas_storage!(energyModel, inputDict, setupDict)
+
+"""
+
 function onsite_gas_storage!(EP::Model, inputs::Dict, setup::Dict)
+    println("Onsite Gas Storage Module")
 
-	println("Onsite Gas Storage Module")
+    dfGen = inputs["dfGen"]  # Dataframe containing generators data
+    T = inputs["T"]          # Number of time steps (hours)
+    Z = inputs["Z"]          # Number of zones
+    p = inputs["hours_per_subperiod"]  # Total number of hours per subperiod
+    GAS_PLANTS = intersect(inputs["THERM_COMMIT"], inputs["RSV"])  # Set of all gas-fired power plants
+    heat_rate = convert(Array{Float64}, inputs["dfGen"][!,:Heat_Rate_MMBTU_per_MWh])  # Heat rate of all resources (million BTUs/MWh)
 
-	dfGen = inputs["dfGen"]
+    # Additional model variables for onsite gas storage
+    @variables(EP, begin
+        vStorageCap[y in GAS_PLANTS] >= 0         # Capacity of storage required by gas-fired resource "y" in MMBtu
+        vStorageCharge[y in GAS_PLANTS, t in 1:T] >= 0  # Amount of gas purchased and stored by resource "y" in hour "t", in MMBtu
+        vStorageDischarge[y in GAS_PLANTS, t in 1:T] >= 0  # Amount of gas/oil discharged from onsite storage of resource "y" in hour "t", in MMBtu
+        vStateOfStorage[y in GAS_PLANTS, t in 1:T] >= 0  # Amount of gas in on-site storage of resource "y" in hour "t", in MMBtu
+    end)
 
-	T = inputs["T"]     # Number of time steps (hours)
-	Z = inputs["Z"]     # Number of zones
+    # Model Expressions
 
-	p = inputs["hours_per_subperiod"] 	# total number of hours per subperiod
+    # Objective Function Expressions
+    # Fixed costs of "on-site gas storage" for resource "y"
+    if setup["CNGStorage"] == 1
+        @expression(EP, eCStorage_fixed[y in GAS_PLANTS], (dfGen[y,:Fixed_CNG_Storage_Cost_per_MMBTUyr] + 1*dfGen[y,:Fixed_CNG_Compressor_Cost_per_MMBtuyr])*vStorageCap[y])
+    end
 
-	GAS_PLANTS = inputs["THERM_ALL"]	# Set of all gas fired power plants
-	
-    # Heat rate of all resources (million BTUs/MWh)
-	heat_rate = convert(Array{Float64}, inputs["dfGen"][!,:Heat_Rate_MMBTU_per_MWh])
-	
-	println("top safe")
-	### Variables ###
+    if setup["OilStorage"] == 1
+        @expression(EP, eCStorage_fixed[y in GAS_PLANTS], dfGen[y,:Fixed_Oil_Storage_Cost_per_MMBTUyr]*vStorageCap[y] + 2938*EP[:vCAP][y])
+    end
 
-	@variables(EP, begin
-			vStorageCap[y in GAS_PLANTS] >= 0 # Capacity of storage required by gas fired resource "y" in MMBtu
-			vStorageCharge[y in GAS_PLANTS, t in 1:T] >=0 # Amount of gas purchased and stored by resource "y" in hour "t", in MMBtu
-			vStorageDischarge[y in GAS_PLANTS, t in 1:T] >=0 # Amount of gas discharged from storage by resource "y" in hour "t", in MMBtu
-			vStateOfStorage[y in GAS_PLANTS, t in 1:T] >=0 # Amount of gas in on-site storage of resource "y" in hour "t", in MMBtu
+    if setup["HydrogenStorage"] == 1
+        @expression(EP, eCStorage_fixed[y in GAS_PLANTS], (dfGen[y,:Fixed_Hydrogen_Storage_Cost_per_MMBTUyr] + 0*dfGen[y,:Fixed_Hydrogen_Compressor_Cost_per_MMBtuyr])*vStorageCap[y])
+    end
 
-	end);
+    # Sum individual resource contributions to fixed costs to get total on-site storage fixed costs
+    @expression(EP, eTotalCStorage_fixed, sum(eCStorage_fixed[y] for y in GAS_PLANTS))
 
-	println("variables safe")
+    # Add total fixed on-site storage cost contribution to the objective function
+    EP[:eObj] += eTotalCStorage_fixed
 
-	### Expressions ###
+    # Variable charging costs of "on-site storage" for resource "y" during hour "t" = fuel cost
+    if setup["CNGStorage"] == 1
+        @expression(EP, eCStorage_var[y in GAS_PLANTS,t=1:T], (inputs["C_Fuel_per_MWh"][y,t]/heat_rate[y])*vStorageCharge[y,t]*inputs["omega"][t])
+    end
 
-	## Objective Function Expressions ##
-	
-	# Fixed costs of "on-site gas storage" for resource "y" 
+    if setup["OilStorage"] == 1
+        @expression(EP, eCStorage_var[y in GAS_PLANTS,t=1:T], dfGen[y,:Oil_Fuel_Cost_per_MMBtu]*vStorageCharge[y,t]*inputs["omega"][t])
+    end
 
-	if setup["CNGStorage"] == 1
-		@expression(EP, eCStorage_fixed[y in GAS_PLANTS], dfGen[y,:Fixed_CNG_Storage_Cost_per_MMBTUyr]*vStorageCap[y] + dfGen[y,:Fixed_CNG_Compressor_Cost_per_yr])
-	end
-	
-	if setup["OilStorage"] == 1
-		@expression(EP, eCStorage_fixed[y in GAS_PLANTS], dfGen[y,:Fixed_Oil_Storage_Cost_per_MMBTUyr]*vStorageCap[y] + dfGen[y,:Fixed_Dual_Oil_Combustor_Cost_per_MWyr]*EP[:vCAP][y])
-	end
+    if setup["HydrogenStorage"] == 1
+        @expression(EP, eCStorage_var[y in GAS_PLANTS,t=1:T], dfGen[y,:Green_Hydrogen_Fuel_Cost_per_MMBtu]*vStorageCharge[y,t]*inputs["omega"][t])
+    end
 
-	if setup["HydrogenStorage"] == 1
-		@expression(EP, eCStorage_fixed[y in GAS_PLANTS], dfGen[y,:Fixed_Hydrogen_Storage_Cost_per_MMBTUyr]*vStorageCap[y] + dfGen[y,:Fixed_Dual_Hydrogen_Combustor_Cost_per_MWyr]*EP[:vCAP][y])
-	end
+    # Sum individual resource contributions to variable charging costs to get total variable charging costs
+    @expression(EP, eTotalCStorage_var, sum(eCStorage_var[y,t] for y in GAS_PLANTS for t in 1:T))
 
-	# Sum individual resource contributions to fixed costs to get total on-site storage fixed costs
-	@expression(EP, eTotalCStorage_fixed, sum(eCStorage_fixed[y] for y in GAS_PLANTS))
-	
-	
-	
-	# Add total fixed on-site storage cost contribution to the objective function
-	EP[:eObj] += eTotalCStorage_fixed
-    
-	
-	
-	# Variable charging costs of "on-site storage" for resource "y" during hour "t" = fuel cost
-	@expression(EP, eCStorage_var[y in GAS_PLANTS,t=1:T], (inputs["C_Fuel_per_MWh"][y,t]/heat_rate[y])*vStorageCharge[y,t]*inputs["omega"][t])
-	
+    # Add total variable discharging cost contribution to the objective function
+    EP[:eObj] += eTotalCStorage_var
 
-	# Sum individual resource contributions to variable charging costs to get total variable charging costs
-	@expression(EP, eTotalCStorage_var, sum(eCStorage_var[y,t] for y in GAS_PLANTS for t in 1:T))
-	
+    # Constraints
 
-	# Add total variable discharging cost contribution to the objective function
-	EP[:eObj] += eTotalCStorage_var
+    # Constraints common to all on-site storage resource y (y in set GAS_PLANTS)
+    @constraints(EP, begin
+        # State of storage constraint
+        cStateOfStorage[y in GAS_PLANTS, t in 1:T], vStateOfStorage[y,t] == vStateOfStorage[y, hoursbefore(p,t,1)] + vStorageCharge[y,t] - vStorageDischarge[y,t]
 
-	println("expressions safe")
-	### Constratints ###
-
-	### Constraints commmon to all on-site storage of resource y (y in set GAS_PLANTS) ###
-	@constraints(EP, begin
-
-
-		# State of storage constraint 
-		cStateOfStorage[y in GAS_PLANTS, t in 1:T], vStateOfStorage[y,t] == vStateOfStorage[y, hoursbefore(p,t,1)] + vStorageCharge[y,t] - vStorageDischarge[y,t]
-
-	end)
-	
-		# #End State of Storage
-		# cStateOfStorageEnd[y in GAS_PLANTS], vStateOfStorage[y,T]==0
-
-	println("first 1 constraint safe")
-
-
-	@constraints(EP, begin
-		# Storage Discharge Constraint
+        # Conditional storage discharge constraint (to ensure that discharge happens by resource y in hour t only when the hourly gas reliability factor g_GR is zero)
         cStorageDischarge[y in GAS_PLANTS, t in 1:T], vStorageDischarge[y,t] == (1-inputs["g_GR"][y,t])*EP[:vP][y,t]*heat_rate[y]
-	
-		# Minimum State of Storage
-		cStateOfStorageMin[y in GAS_PLANTS, t in 1:T], vStateOfStorage[y,t] >= vStorageDischarge[y,t] + EP[:vRSV][y,t]
 
-		# Maximum State of Storage
-		cStateOfStorageMax[y in GAS_PLANTS, t in 1:T], vStateOfStorage[y,t] <= vStorageCap[y]
-	
-		#Maximum Storage Charge rate
-		cStorageChargeMax[y in GAS_PLANTS, t in 1:T], vStorageCharge[y,t] <= dfGen[y,:CNG_Storage_Max_Charge_Rate_MMBtu_per_hr]*inputs["g_GR"][y,t]
-	
-		#Gas conver
-		# # Big-M Storage Charge Constraint
-		# cStorageChargeBM[y in GAS_PLANTS, t in 1:T], vStorageCharge[y,t] <= *100000 
-	end)
+        # Minimum state of storage constraint (to meet both discharge and reserve requirements by each resource y in each hour t) 
+        cStateOfStorageMin[y in GAS_PLANTS, t in 1:T], vStateOfStorage[y,t] >= vStorageDischarge[y,t] + EP[:vRSV][y,t]*heat_rate[y]
 
-	println("constraints safe")
-	println("Successfully read onsite_gas_storage module")
+        # Maximum State of Storage constraint (to ensure the level of storage in each hour t by storage resource of y does not exceed the built storage capacity of y)
+        cStateOfStorageMax[y in GAS_PLANTS, t in 1:T], vStateOfStorage[y,t] <= vStorageCap[y]
+    end)
+
+    # Maximum storage charge rate by resource y in each hour t
+    if setup["CNGStorage"] == 1
+		@constraints(EP, begin
+        	cStorageChargeMax[y in GAS_PLANTS, t in 1:T], vStorageCharge[y,t] <= dfGen[y,:CNG_Storage_Max_Charge_Rate_MMBtu_per_hr]*inputs["g_GR"][y,t]
+		end)
+    end
+
+    if setup["OilStorage"] == 1
+		@constraints(EP, begin
+        	cStorageChargeMax[y in GAS_PLANTS, t in 1:T], vStorageCharge[y,t] <= dfGen[y,:Oil_Storage_Max_Charge_Rate_MMBtu_per_hr]*inputs["g_GR"][y,t]
+		end)
+    end
+
+    if setup["HydrogenStorage"] == 1
+		@constraints(EP, begin
+        	cStorageChargeMax[y in GAS_PLANTS, t in 1:T], vStorageCharge[y,t] <= dfGen[y,:Hydrogen_Storage_Max_Charge_Rate_MMBtu_per_hr]*inputs["g_GR"][y,t]
+		end)
+    end
+
+    println("Successfully read onsite_gas_storage module")
 end
-	
--- src/write_outputs/write_onsite_gas_storage.jl
+++ src/write_outputs/write_onsite_gas_storage.jl
@@ -1,23 +1,58 @@
 @doc raw"""
-	write_storage(path::AbstractString, inputs::Dict,setup::Dict, EP::Model)
+    write_onsite_gas_storage(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
 
-Function for writing the capacities of different storage technologies, including hydro reservoir, flexible storage tech etc.
+Writes the onsite gas storage data of the optimization model to CSV files.
+
+## Description
+This function extracts and writes data related to onsite gas storage from an energy production model `EP` to CSV files. It handles data such as built storage capacities, hourly charges, discharges, and states of charge for gas plants.
+
+## Arguments
+- `path::AbstractString`: The file path where the CSV files will be saved.
+- `inputs::Dict`: A dictionary containing input data required for the function, including:
+  - `dfGen`: Dataframe with generators data.
+  - `T`: Number of time steps.
+  - `G`: Gas plants identifiers.
+  - `RESOURCES`: Resource identifiers.
+  - Other necessary inputs.
+- `setup::Dict`: A dictionary containing setup configurations (not directly used in this function).
+- `EP::Model`: The energy production optimization model containing gas storage data.
+
+## Output
+Creates and saves four CSV files at the specified `path`:
+- `built_gas_storage_capacities.csv`: Contains built storage capacities for each resource.
+- `hourly_gas_storage_charges.csv`: Contains hourly gas storage charges for each resource.
+- `hourly_gas_storage_discharges.csv`: Contains hourly gas storage discharges for each resource.
+- `hourly_gas_storage_states_of_charge.csv`: Contains hourly states of charge for each resource.
+
+## Example
+```julia
+write_onsite_gas_storage("path/to/directory", inputDict, setupDict, energyModel)
+
 """
-function write_onsite_gas_storage(path::AbstractString, inputs::Dict,setup::Dict, EP::Model)
-	dfGen = inputs["dfGen"]
-	T = inputs["T"]     # Number of time steps (hours)
-	G = inputs["G"]
-	GAS_PLANTS = inputs["THERM_ALL"]
-	
-	built_gas_storage_capacities = DataFrame(Resource = inputs["RESOURCES"][GAS_PLANTS], Built_Storage_CAP = value.(EP[:vStorageCap][GAS_PLANTS]).data)
-	println("built_gas_storage_capacities pass")
-	hourly_gas_storage_charges = DataFrame(transpose(value.(EP[:vStorageCharge]).data), inputs["RESOURCES"][GAS_PLANTS])
-	println("hourly_gas_storage_charges pass")
-	hourly_gas_storage_discharges = DataFrame(transpose(value.(EP[:vStorageDischarge]).data), inputs["RESOURCES"][GAS_PLANTS])
-	println("hourly_gas_storage_discharges pass")
-	hourly_gas_storage_states_of_charge = DataFrame(transpose(value.(EP[:vStateOfStorage]).data), inputs["RESOURCES"][GAS_PLANTS])
-	println("hourly_gas_storage_states_of_charge pass")
 
+function write_onsite_gas_storage(path::AbstractString, inputs::Dict,setup::Dict, EP::Model)
+
+	dfGen = inputs["dfGen"]
+	T = inputs["T"]  
+	G = inputs["G"]
+	GAS_PLANTS = intersect(inputs["THERM_COMMIT"], inputs["RSV"])
+
+	# Building DataFrame for built gas storage capacities
+	built_gas_storage_capacities = DataFrame(Resource = inputs["RESOURCES"][GAS_PLANTS], Built_Storage_CAP = value.(EP[:vStorageCap][GAS_PLANTS]).data)
+
+	# Building DataFrame for hourly gas storage charges
+	hourly_gas_storage_charges = DataFrame(transpose(value.(EP[:vStorageCharge]).data), inputs["RESOURCES"][GAS_PLANTS])
+	insertcols!(hourly_gas_storage_charges, 1, :Time_index => 1:T)
+
+	# Building DataFrame for hourly gas storage discharges
+	hourly_gas_storage_discharges = DataFrame(transpose(value.(EP[:vStorageDischarge]).data), inputs["RESOURCES"][GAS_PLANTS])
+	insertcols!(hourly_gas_storage_discharges, 1, :Time_index => 1:T)
+
+	# Building DataFrame for hourly gas storage states of charge
+	hourly_gas_storage_states_of_charge = DataFrame(transpose(value.(EP[:vStateOfStorage]).data), inputs["RESOURCES"][GAS_PLANTS])
+	insertcols!(hourly_gas_storage_states_of_charge, 1, :Time_index => 1:T)
+
+	# Writing DataFrames to CSV files at the specified path
 	CSV.write(joinpath(path, "built_gas_storage_capacities.csv"), built_gas_storage_capacities)
 	CSV.write(joinpath(path, "hourly_gas_storage_charges.csv"), hourly_gas_storage_charges)
 	CSV.write(joinpath(path, "hourly_gas_storage_discharges.csv"), hourly_gas_storage_discharges)
